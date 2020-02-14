1af43942a0a7f779cdaa07bd1a6df157e1f660e4
king-del-north <alanvera479@gmail.com>
2020-02-13 21:27:53 -0300

    navBasico

---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import  React from "react";
+
+
+class Navbar extends React.Component{
+
+render(){
+    return(
+      <div>
+        <h1>
+            hola soy navbar
+            
+        </h1>
+        <p>
+            hola soy navbar
+        </p>
+      </div>
+      
+    );
+}
+}
+
+export default Navbar;
